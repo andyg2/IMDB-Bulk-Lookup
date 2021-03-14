15253ff8136416b67bf77ca438c69879568e3c80
andy <rehit.it@gmail.com>
2021-03-14 23:41:31 +0800

    Refactoring fork to work with omdbapi.com free IMDB API

--- README.md
+++ README.md
@@ -1,26 +1,43 @@
 IMDB-Bulk-Lookup
 ================
 
-A Node.js program that you pass in a file containing movie titles and it looks them up on 
+A Node.js program that you pass in a file containing movie titles and an api key and it looks them up on 
 IMDB and brings back the rating and other stuff. Uses the imdbapi.org API to perform the search.
 
 It ain't perfect but it doesn't do a bad job. Give it good titles and it will do a better job.
 
-Run it like this:
+Run it like this: 
 
-	> node imdb-bulk-lookup sample_movies.txt
+	> node imdb-bulk-lookup sample_movies.txt ab123456
+    
+Were *ab123456* is your api key for https://www.omdbapi.com/apikey.aspx FREE! (1,000 daily limit)
 
-And it will respond with this:
+Where *sample_movies.txt* format is a tab separated list of movie title and year  - the year is optional
 
-	Star Wars A New Hope, Star Wars, 8.8, PG, Action,Adventure,Fantasy,Sci-Fi
-	404: Film not found: This title doesn't exist
-	28 days later, 28 Days Later..., 7.6, R, Horror,Sci-Fi,Thriller
+
+    > Scream Test    [tab] 2020
+    > Soul           [tab] 2020
+    > The Dissident  [tab] 2020
+
+
+Or a movie name with the year in parenthesis
+
+    > Scream Test (2020)
+    > Soul (2020)
+    > The Dissident (2020)
+
+And it will respond with the movie title and IMDB rating while building a CSV spreadsheet named: [timestamp]-results.csv
+
+    Scream Test (2020) 8.6
+    Soul (2020) 8.1
+    The Dissident (2020) 8.1
+
 
 The colunmns it returns are
 
-	The name you searched for, the title it found, the score, the rating, a list of genres.
+Title,	Year,	imdbID,	Genre,	Rated,	Released,	Runtime,	Director,	Writer,	Actors,	Plot,	Language,	Country,	Awards,	Poster,	Metascore,	imdbRating,	imdbVotes,	Type,	DVD,	BoxOffice,	Production,	Website,	Response
 
 I'm open to suggestions for new features so feel free to submit pulls.
 
-Enjoy,
-Simon Raik-Allen.
+Enjoy, Andy
+Thanks to Simon Raik-Allen for the parent repo
--- imdb-bulk-lookup.js
+++ imdb-bulk-lookup.js
@@ -1,27 +1,31 @@
-// IMDB Bulk Lookup 
-// By Simon Raik-Allen
+// IMDB Bulk Lookup
+// By Simon Raik-Allen and Andy Gee
 
 var fs = require('fs');
-var http = require('http');
+var https = require('https');
 
-var SEND_PAUSE = 1200;		// time to wait between sending lookup requests
+var SEND_PAUSE = 500; // time to wait between sending lookup requests
+
+var csvfile = Date.now() + "-results.csv";
+var headings = ["Title", "Year", "imdbID", "Genre", "Rated", "Released", "Runtime", "Director", "Writer", "Actors", "Plot", "Language", "Country", "Awards", "Poster", "Metascore", "imdbRating", "imdbVotes", "Type", "DVD", "BoxOffice", "Production", "Website", "Response"];
+fs.appendFileSync(csvfile, headings.join(","));
 
 var options = {
-  host: 'imdbapi.org',
-  path: '/'
+	host: 'www.omdbapi.com'
 };
 
-var fileName = process.argv[2];
-if (!fileName) {
-	console.log("Usage: node " + process.argv[1] + " <filename>")
+var API_KEY = process.argv[3]; // https://www.omdbapi.com/  FREE! (1,000 daily limit)
+
+if (!API_KEY) {
+	console.log("Usage: node " + process.argv[1] + " <filename> <omdb api key>")
 	process.exit(1);
 }
+var fileName = process.argv[2];
 
 // Get the movie names from file
 var movieNames;
 try {
 	movieNames = fs.readFileSync(fileName).toString().split("\n");
-	movieNames.pop(); // no idea why there is an extra line at the end
 } catch (e) {
 	console.log(e.message);
 	process.exit(1);
@@ -29,22 +33,49 @@ try {
 
 // step through the list
 function processMovieName(i) {
-	if (i >= movieNames.length) return;
+	if (i >= movieNames.length)
+		return;
 
-    options.path = "/?limit=5&q=" + encodeURIComponent(movieNames[i]);
+	if (!movieNames[i].indexOf("\t")) { // if no tab then try to find year inside ()
+		var pos = movieNames[i].lastIndexOf("(");
+		if (pos) {
+			movieNames[i] = movieNames[i].slice(0, pos) + "\t" + movieNames[i].slice(pos + 1);
+		}
+	}
 
-    // ask IMDB to search for the title
-	http.get(options, processResponse(movieNames[i])).on('error', function(e) {
-  		console.log('ERROR: ' + movieNames[i] + "::" + e.message);
-	});
+	var parts = movieNames[i].split("\t"); // format, [title][tab][year] or just [title]
 
-	// pause a little before sending so we don't blow the API throttling limit
-	setTimeout(next(i+1), SEND_PAUSE);
+	options.path = false;
+
+	if (parts[0].length) { // check title length isn't empty
+		options.path = "/?apikey=" + API_KEY + "&t=" + encodeURIComponent(parts[0]);
+		if (parts.length == 2) { // look for year
+			parts[1] = parseInt(parts[1].replace(/[^0-9]+/g, ""));
+
+			if (parts[1] > 1000 && parts[1] < 3000) { // check year is between 1000 and 3000
+				options.path = "/?apikey=" + API_KEY + "&t=" + encodeURIComponent(parts[0]) + "&y=" + parts[1];
+			}
+		}
+	}
+
+	if (options.path) {
+		// console.log(options);
+		// ask IMDB to search for the title
+		https.get(options, processResponse(movieNames[i])).on('error', function (e) {
+			console.log('ERROR: ' + movieNames[i] + "::" + e.message);
+		});
+
+		// pause a little before sending so we don't blow the API throttling limit
+		setTimeout(next(i + 1), SEND_PAUSE);
+
+	}
+
 }
 
 function next(i) {
 	var i = i;
 	return function () {
+
 		processMovieName(i);
 	}
 }
@@ -52,37 +83,36 @@ function next(i) {
 function processResponse(name) {
 	var movieName = name;
 
-	return function(response) {
-  		if (response.statusCode == 200) {
+	return function (response) {
+		if (response.statusCode == 200) {
 			var body = '';
-	  		response.on('data', function (chunk) {
-	    		body += chunk;
-	  		});
-	  		response.on('end', function () {
-	  			body = JSON.parse(body);
+			response.on('data', function (chunk) {
+				body += chunk;
+			});
+			response.on('end', function () {
+				body = JSON.parse(body);
+				var columns = [];
 
-	  			if (body.code) {
-	  				console.log(body.code + ": " + body.error + ": " + movieName);
-	  			} else {
-	  				// loop over each of the possible 5 results
-	  				var theMovie = body[0];
-	  				if (body.length > 1) {
-		  				for (j in body) {
-		  					if (body[j].title == movieName) {
-		  						theMovie = body[j];
-		  					}
-		    			}
+				headings.forEach((heading, index) => {
+					if (body[heading] && body[heading].length) {
+						// console.log(heading,body[heading]);
+						columns.push('"' + body[heading].replace(/"/g, '\"') + '"');
+					} else {
+						columns.push('');
 					}
-					// we now have the movie
-    				console.log(movieName + ", " +theMovie.title + ", " + theMovie.rating + ", " + theMovie.rated + ", " + theMovie.genres);
-	    		}
-	  		});
-	  	} else {
-	  		console.log("ERROR: " + response.statusCode);
-	  	}
+				});
+
+				if (columns.length) {
+					fs.appendFileSync(csvfile, columns.join(",") + "\r\n");
+					console.log(name, body.imdbRating);
+				}
+
+			});
+		} else {
+			console.log("ERROR: " + response.statusCode);
+		}
 	}
 }
 
 // start
 processMovieName(0);
-
